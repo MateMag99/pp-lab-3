fa1ee84c421771608ecdcb9fe346f8e88eaa6cf2
MateMag99 <matemag99@gmail.com>
2024-04-06 18:32:56 +0200

    zad1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Point point = new Point(5,10);
+        System.out.println(point);
     }
 }
