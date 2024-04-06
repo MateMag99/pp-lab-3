3a1d8fda93ed413dca2ef91fad76a11a90709a6a
MateMag99 <matemag99@gmail.com>
2024-04-06 19:01:01 +0200

    zad4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,5 +15,20 @@ public class Main {
 
         ColoredCircle coloredCircle = new ColoredCircle(2,4, "RED");
         System.out.println(coloredCircle);
+
+        Circle[] tab = new Circle[5];
+        tab[0] = new Circle(1,3);
+        tab[1] = new ColoredCircle(2,5, "BLUE");
+        tab[2] = new Circle(1,7);
+        tab[3] = new ColoredCircle(5,12, "RED");
+        tab[4] = new Circle(3,9);
+
+        for (int i = 0; i < tab.length; i++) {
+            if(tab[i] instanceof ColoredCircle) {
+                System.out.println(tab[i].calculateArea() + " " + ((ColoredCircle) tab[i]).getColor());
+            } else {
+                System.out.println(tab[i].calculateArea());
+            }
+        }
     }
 }
